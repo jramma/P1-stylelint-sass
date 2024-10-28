bbffa75c84431489006e9da2b63eaf3fec228536
jramma <pepe.ramos@mortensen.cat>
2024-10-28 21:48:55 +0100

    swiper

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,14 +3,40 @@
  * see commented examples below
  */
 
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
+const swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3.5,
+      spaceBetween: 50,
+    },
+  },
+});
 
 /**
  * Write any other JavaScript below
  */
 
-+( function() {
++(function () {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
-} )();
+})();
